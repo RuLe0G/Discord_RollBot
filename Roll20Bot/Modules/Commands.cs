6f669f65ebbd96ca1dc6b431244558d2a6a673dc
RuLe0G <khlebnikov2015@yandex.ru>
2020-10-26 18:33:39 +0700

    Initial commit

---
+++ Roll20Bot/Modules/Commands.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Discord;
+using Discord.Commands;
+
+namespace Roll20Bot.Modules
+{
+    public class Commands : ModuleBase<SocketCommandContext>
+    {
+        Random random = new Random();
+
+        private Color Clr = Color.Gold;
+        private string txt;
+        private string[] txtPack = new string[10] {
+            "Отличный бросок",
+            "Можно и лучше",
+            "Такова судьба",
+            "4",
+            "5",
+            "6",
+            "7", 
+            "8", 
+            "9", 
+            "10", 
+        };
+
+
+
+        [Command("roll")]
+        [Alias("Roll", "R", "r", "dice", "Dice", "D", "d")]
+        public async Task roll(string InputStr)
+        {
+            Embed embed;
+            var quantityXfacets = HelpRoll(InputStr);
+
+            if (quantityXfacets.Key < 1 || quantityXfacets.Value < 1)
+            {
+                var EmbedBuilder = new EmbedBuilder()
+                .WithColor(Clr)
+                .WithTitle("Неправильный формат запроса. Пример запроса **xxdyy**, где x - число кубов, а y - кол-во граней.")
+                .WithFooter(footer =>
+                {
+                    footer
+                    .WithText("Воспользуйтесь `!help`");
+                });
+                embed = EmbedBuilder.Build();
+            }
+            else
+            {
+                int r = 0;
+                for (int i = 0; i < quantityXfacets.Key; i++)
+                {
+                    r += random.Next(1, quantityXfacets.Value+1);
+                }
+                
+
+                if (r == 1)
+                {
+                    Clr = Color.Red;
+                    txt = "КРИТИЧЕСКИЙ ПРОВАЛ";
+                }
+                else if (r == quantityXfacets.Value * quantityXfacets.Key)
+                {
+                    Clr = Color.Blue;
+                    txt = "КРИТИЧЕСКИЙ УСПЕХ";
+                }
+                else
+                {
+                    Clr = Color.Green;
+                    txt = txtPack[random.Next(0, 10)];
+                }
+
+
+                var EmbedBuilder = new EmbedBuilder()
+                    .WithColor(Clr)
+                    .WithTitle("Бросок - " + r.ToString() + "!")
+                    .WithFooter(footer =>
+                        {
+                            footer
+                            .WithText(txt);
+                        });
+                embed = EmbedBuilder.Build();
+            }
+            
+            await ReplyAsync(embed: embed);
+        }
+
+        private KeyValuePair<int, int> HelpRoll(string inputStr)
+        {
+            int q = -1, f = -1;
+
+            inputStr = inputStr.ToLower();
+            if (inputStr.Contains('d'))
+            {
+                q = Convert.ToInt32(inputStr.Split('d')[0]);
+                f = Convert.ToInt32(inputStr.Split('d')[1]);
+                if (q < 1)
+                    q *= -1;
+                if (f < 1)
+                    f *= -1;
+            }
+            if (inputStr.Contains('к'))
+            {
+                q = Convert.ToInt32(inputStr.Split('к')[0]);
+                f = Convert.ToInt32(inputStr.Split('к')[1]);
+                if (q < 1)
+                    q *= -1;
+                if (f < 1)
+                    f *= -1;
+            }
+
+            return new KeyValuePair<int, int>(q, f);
+        }
+
+        [Command("help")]
+        [Alias("Help", "h", "H", "Помощь", "П", "помощь", "п")]
+        public async Task Help()
+        {
+            Embed embed;
+            var EmbedBuilder = new EmbedBuilder()
+                   .WithColor(Color.Teal)
+                   .WithTitle("Команды")
+                   .WithFooter(footer =>
+                   {
+                       footer
+                        .WithText("!r xxxdyyy - бросок кубиков (x - число кубов, а y - кол-во граней)." +
+                        "\n!h - памагат" +
+                        "\n!book - даст вам ссылку на книжку на гугл диске" +
+                        "\n!conv - Конвертер монет из DnD" +
+                        "\n что-то может быть когда-нить или вообще ничего не будет и я уничтожу эту строку");
+                   });
+            embed = EmbedBuilder.Build();
+            await ReplyAsync(embed: embed);
+        }
+    }
+}
---
+++ Roll20Bot/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Roll20Bot
+{
+    class Program
+    {
+        static void Main(string[] args) => new Program().RunTaskAsync().GetAwaiter().GetResult();
+
+        private DiscordSocketClient client;
+        private CommandService commands;
+        private IServiceProvider services;
+
+        public async Task RunTaskAsync()
+        {
+            client = new DiscordSocketClient();
+            commands = new CommandService();
+            services = new ServiceCollection()
+                .AddSingleton(client)
+                .AddSingleton(commands)
+                .BuildServiceProvider();
+
+            string token = "NzcwMTc4ODQzNzM3OTgwOTM5.X5Zy8Q.W-FazTtPvp3TsfOqXFNjGGl4a1A";
+
+            client.Log += Client_Log;
+
+            await RegisterComandsAsync();
+
+            await client.LoginAsync(TokenType.Bot, token);
+
+            await client.StartAsync();
+
+            await Task.Delay(-1);
+        }
+
+        private Task Client_Log(LogMessage arg)
+        {
+            Console.WriteLine(arg);
+            return Task.CompletedTask;
+        }
+
+        public async Task RegisterComandsAsync()
+        {
+            client.MessageReceived += HandleCommandsAsync;
+            await commands.AddModulesAsync(Assembly.GetExecutingAssembly(), services);
+        }
+
+        private async Task HandleCommandsAsync(SocketMessage arg)
+        {
+            var message = arg as SocketUserMessage;
+            var context = new SocketCommandContext(client, message);
+            if (message.Author.IsBot) return;
+
+            int argPos = 0;
+            if (message.HasStringPrefix("!", ref argPos))
+            {
+                var result = await commands.ExecuteAsync(context, argPos, services);
+                if (!result.IsSuccess) Console.WriteLine(result.ErrorReason);  
+            }
+        }
+    }
+}
